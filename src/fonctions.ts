ad23e4704559b2d8169ced61c17bd20eb60fb341
julienmalard <julien.malard@mail.mcgill.ca>
2025-05-29 15:40:49 +0530

    Ajouter fonction `obtenir`

--- src/fonctions.ts
+++ src/fonctions.ts
@@ -7,8 +7,78 @@ import type {
   PasNondéfini,
   élémentsBd,
 } from "@/types.js";
+import { isNull } from "lodash-es";
 import { AbortError } from "p-retry";
+
 import type { Journal } from "./types.js";
+import type { TypedEmitter } from "tiny-typed-emitter";
+
+export const obtenir = async <T>(
+  f: (args: {
+    si: (f: (x: T) => boolean) => (x: T) => void;
+    siDéfini: () => (x: T | undefined) => void;
+    siVide: () => (x: T) => void;
+    siNul: () => (x: T) => void;
+    siPasVide: () => (x: T) => void;
+    siPasNul: () => (x: T) => void;
+    tous: () => (x: T) => void;
+  }) => Promise<schémaFonctionOublier>,
+): Promise<T> => {
+  const événements = new TypedEmitter<{ résolu: (x: T) => void }>();
+  const si = (fTest: (x: T) => boolean): ((x: T) => void) => {
+    return (x: T) => {
+      const passe = fTest(x);
+      if (passe) {
+        événements.emit("résolu", x);
+      }
+    };
+  };
+  const siDéfini = (): ((x: T | undefined) => void) => {
+    // @ts-expect-error Je ne sais pas pourquoi
+    return si((x: T | undefined): x is T => x !== undefined);
+  };
+  const siVide = (): ((x: T) => void) => {
+    return si((x) => {
+      if (Array.isArray(x)) return x.length === 0;
+      else if (typeof x === "object" && !isNull(x))
+        return Object.keys(x).length === 0;
+      else return false;
+    });
+  };
+  const siNul = (): ((x: T) => void) => {
+    return si((x: T) => isNull(x));
+  };
+  const siPasVide = (): ((x: T) => void) => {
+    return si((x) => {
+      if (Array.isArray(x)) return x.length > 0;
+      else if (typeof x === "object" && !isNull(x))
+        return Object.keys(x).length > 0;
+      else return false;
+    });
+  };
+  const siPasNul = (): ((x: T) => void) => {
+    return si((x: T) => !isNull(x));
+  };
+  const tous = (): ((x: T) => void) => {
+    return si(() => true);
+  };
+
+  const promesse = new Promise<T>((résoudre) =>
+    événements.once("résolu", résoudre),
+  );
+  const fOublier = await f({
+    si,
+    siDéfini,
+    siVide,
+    siNul,
+    siPasVide,
+    siPasNul,
+    tous,
+  });
+  after(async () => await fOublier());
+
+  return promesse;
+};
 
 class ÉmetteurUneFois<T> extends EventEmitter {
   condition: (x: T) => boolean | Promise<boolean>;
